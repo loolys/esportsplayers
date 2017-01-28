c17bd4aa796c367a9f9fe2f0d110febb6768ea70
loolys <pierre.liljekvist@outlook.com>
2017-01-28 10:33:47 +0100

    Made node handle server side

--- src/components/playerPreview.js
+++ src/components/playerPreview.js
@@ -3,17 +3,17 @@ import React from 'react';
 import {Link} from 'react-router';
 
 export default class PlayerPreview extends React.Component {
-	render() {
-		return ( 
-			<Link to={`/player/${this.props.id}`}>
-				<div className="player-preview">
-					<img src={`img/${this.props.image}`} />
-					<h2 className="name">{this.props.name}</h2>
-					<span className="medals-count">
-						<img src="/img/medal.png" />{this.props.length}
-					</span>
-				</div>
-			</Link>
-		);
-	}
+  render() {
+    return ( 
+      <Link to={`/player/${this.props.id}`}>
+        <div className="player-preview">
+          <img src={`img/${this.props.image}`} />
+          <h2 className="name">{this.props.name}</h2>
+          <span className="medals-count">
+            <img src="/img/medal.png" />{this.props.achievments.length}
+          </span>
+        </div>
+      </Link>
+    );
+  }
 }
